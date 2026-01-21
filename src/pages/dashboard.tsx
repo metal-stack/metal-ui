590e5bca8bf332b4a979625ac7203ac2ddfd69e6
ostempel <github_os@posteo.de>
2026-01-21 13:26:53 +0100

    add dashboard permissions content

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,11 @@
+import CodeBlock from "@/components/code-block/code-block";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { useMethods } from "@/providers/MethodsProvider";
 import { TriangleAlertIcon } from "lucide-react";
 
 export default function Dashboard() {
+  const methodsCtx = useMethods();
+
   return (
     <div className="flex flex-col gap-4 md:gap-6">
       <Alert className="text-primary-foreground bg-primary">
@@ -11,6 +15,11 @@ export default function Dashboard() {
           This ui is under construction.
         </AlertDescription>
       </Alert>
+      <CodeBlock data={methodsCtx.tokenScope} title="Token scope" />
+      <CodeBlock
+        data={methodsCtx.methodsPermissions}
+        title="Methods permissions"
+      />
     </div>
   );
 }
